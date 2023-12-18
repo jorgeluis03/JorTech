f04ee161da1995f244a32bcb082b5c34f3684fed
jorgeluis <jorgel.dominguez03@gmail.com>
2023-12-18 13:25:56 -0500

    necesito el plan premiun para tener las libertar con las respuestas

--- app/src/main/java/com/example/chatbot/activities/MainActivity.java
+++ app/src/main/java/com/example/chatbot/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.chatbot.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -14,12 +16,22 @@ import com.example.chatbot.adapter.MessageAdapter;
 import com.example.chatbot.databinding.ActivityMainBinding;
 import com.example.chatbot.model.MessageModel;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -29,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     MessageAdapter adapter;
     List<MessageModel> messageList;
     public static final MediaType JSON = MediaType.get("application/json");
+
+    OkHttpClient client = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,8 +79,50 @@ public class MainActivity extends AppCompatActivity {
         messageList.add(new MessageModel("...",MessageModel.SENT_BY_BOT));
 
         JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("model","text-davinci-003");
+            jsonObject.put("prompt",inputQuestion);
+            jsonObject.put("max_tokens",5000);
+            jsonObject.put("temperature",0);
 
 
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        RequestBody requestBody = RequestBody.create(jsonObject.toString(),JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponde("Error: "+e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response){
+                if(response.isSuccessful()){
+                    Log.d("msg-test","success");
+
+                }else {
+                    try {
+                        String errorBody = response.body().string();
+                        Log.d("msg-test", "Error: " + errorBody);
+                    } catch (IOException e) {
+                        Log.e("msg-test", "Error reading error body", e);
+                    }
+                }
+            }
+        });
+    }
+
+    public void addResponde(String s){
+        messageList.remove(messageList.size()-1);
+        addToChat(s,MessageModel.SENT_BY_BOT);
     }
 
     private void addToChat(String inputQuestion, String sentByMe) {
